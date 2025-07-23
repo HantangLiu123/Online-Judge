182b4b34a7cba780d86fb989310eb6ca3072f4bb
HantangLiu123 <hantang.liu@mail.utoronto.ca>
2025-07-23 10:00:03 +0800

    fix: fixed the issue of getting 404 from all the langauge requests

--- oj_app/api/api.py
+++ oj_app/api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from .endpoints import problems_manager, authorization, users
+from .endpoints import problems_manager, authorization, users, languages
 
 # set the prefix for api, and add all routers
 router = APIRouter(prefix='/api')
 router.include_router(problems_manager.router)
 router.include_router(authorization.router)
 router.include_router(users.router)
+router.include_router(languages.router)
