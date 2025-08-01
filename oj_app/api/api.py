447f462db4b6e5b56d049eba4aaea8e66ed46acd
HantangLiu123 <hantang.liu@mail.utoronto.ca>
2025-08-01 18:50:54 +0800

    fix: added the router of import_data

--- oj_app/api/api.py
+++ oj_app/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from .endpoints import problems_manager, authorization, users, languages, submissions
-from .endpoints import reset, export_data
+from .endpoints import reset, export_data, import_data
 
 # set the prefix for api, and add all routers
 router = APIRouter(prefix='/api')
@@ -11,3 +11,4 @@ router.include_router(languages.router)
 router.include_router(submissions.router)
 router.include_router(reset.router)
 router.include_router(export_data.router)
+router.include_router(import_data.router)
