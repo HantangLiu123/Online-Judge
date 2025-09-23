97335f3df286137dc1429dfcfc96cf17dd180cc7
HantangLiu123 <hantang.liu@mail.utoronto.ca>
2025-09-22 21:10:08 -0400

    fix: the users endpoints are now added to the router

--- backends/api/router/api_router.py
+++ backends/api/router/api_router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .endpoints import (
     authorization,
+    users,
 )
 
 oj_router = APIRouter(prefix='/api')
 oj_router.include_router(authorization.router)
+oj_router.include_router(users.router)
