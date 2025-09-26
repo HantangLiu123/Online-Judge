97c40ed0e415287a1afa850424e30dcd6a8d35cb
HantangLiu123 <hantang.liu@mail.utoronto.ca>
2025-09-25 22:21:12 -0400

    perf: there is no psql command in the api container, and there is a better way to avoid double init

--- backends/api/run.sh
+++ backends/api/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if psql -h db -U postgres -d mydb -tAc "SELECT to_regclass('public.aerich');" | grep -q aerich; then
-    echo "Running migrations..."
-    /opt/conda/envs/Online_Judge/bin/aerich upgrade
-else
-    echo "Initializing database..."
-    /opt/conda/envs/Online_Judge/bin/aerich init-db
-fi
+/opt/conda/envs/Online_Judge/bin/aerich init-db || /opt/conda/envs/Online_Judge/bin/aerich upgrade
 
 /opt/conda/envs/Online_Judge/bin/uvicorn api.OJ-main:app --host 0.0.0.0 --port 8000 --reload --no-access-log
