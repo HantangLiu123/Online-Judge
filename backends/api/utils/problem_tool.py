3f3d2dfacc19c5417ee62ee217a15bbfe1590553
HantangLiu123 <hantang.liu@mail.utoronto.ca>
2025-09-28 10:53:13 -0400

    refactor: the pagination logic is put in the shared_tool now so that all endpoints need pagination can use it

---
+++ backends/api/utils/problem_tool.py
@@ -0,0 +1,51 @@
+import asyncio
+from typing import Literal
+from . import shared_tool
+from shared.models import Problem
+from shared.utils import oj_cache, problem_parse
+
+async def problem_list_paginated(
+    page: int,
+    page_size: int,
+    hardness: Literal['easy', 'medium', 'hard'] | None = None,
+) -> tuple[int, int, list[dict]]:
+    
+    """get the problem list paginated"""
+
+    if hardness:
+        # if there is hardness filter, tells the function
+        total, total_page, problems = await shared_tool.get_list_paginated(
+        type=Problem,
+        page=page,
+        page_size=page_size,
+        needed_info=['id', 'title'],
+        hardness=hardness,
+        )
+    else:
+        # if not, 
+        total, total_page, problems = await shared_tool.get_list_paginated(
+            type=Problem,
+            page=page,
+            page_size=page_size,
+            needed_info=['id', 'title'],
+        )
+
+    # store the cache map
+    map_tasks = [oj_cache.store_info_key_map(
+        item_type='problem',
+        cache_key=oj_cache.problem_list_key(page, page_size, hardness),
+        expire=120,
+        problem_id=problem['id'],
+    ) for problem in problems]
+    await asyncio.gather(*map_tasks)
+
+    return total, total_page, problems
+
+def dump_testcases(problem: Problem) -> dict:
+
+    """this function dumps the testcases in a problem away"""
+
+    problem_info = problem_parse.problem_to_problem_schema(problem)
+    problem_dict: dict = problem_info.model_dump()
+    del problem_dict['testcases']
+    return problem_dict
---
+++ backends/api/utils/shared_tool.py
@@ -0,0 +1,47 @@
+from typing import Type
+from tortoise import models
+from fastapi import HTTPException, status
+
+async def get_list_paginated(
+    type: Type[models.Model],
+    page: int,
+    page_size: int,
+    needed_info: list[str],
+    **kwargs,
+) -> tuple[int, int, list[dict]]:
+    
+    """get the paginated list according to the params"""
+
+    # get the total number
+    if kwargs:
+        total = await type.filter(**kwargs).count()
+    else:
+        total = await type.all().count()
+
+    if total == 0:
+        # returns a blank list
+        return 0, 0, []
+    
+    total_pages = (total + page_size - 1) // page_size
+    if page > total_pages:
+        # the page does not exist
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                'code': status.HTTP_404_NOT_FOUND,
+                'msg': 'the page does not exist',
+                'data': None,
+            }
+        )
+    
+    offset = (page - 1) * page_size
+    if kwargs:
+        return_list = await type.filter(**kwargs).offset(offset).limit(page_size).values(
+            *needed_info
+        )
+    else:
+        return_list = await type.all().offset(offset).limit(page_size).values(
+            *needed_info
+        )
+
+    return total, total_pages, return_list
